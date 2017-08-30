4682965ddb1f8e389d744322680ba5e4abe1fad7
Harrison Klein <bassismything9@mierda.local>
2017-08-29 22:32:38 -0400

    tuesday night

--- PracticeMe/PracticeMe/ViewController.swift
+++ PracticeMe/PracticeMe/ViewController.swift
@@ -22,9 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func createNewGoal(_ sender: Any) {
+        let alert = UIAlertController(title: "Select A Type", message: nil, preferredStyle: .actionSheet)
+        let goalAction = UIAlertAction(title: "Goal", style: .default) {
+            (alert: UIAlertAction!) -> Void in
+            self.data.append(Cal_Goal_Data(cal_Goal_Type: .goal))
+        }
+        let auditionAction = UIAlertAction(title: "Audition", style: .default) {
+            (alert: UIAlertAction!) -> Void in
+            self.data.append(Cal_Goal_Data(cal_Goal_Type: .audition))
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
-        let newGoal = Cal_Goal_Data()
-        data.append(newGoal)
+        alert.addAction(goalAction)
+        alert.addAction(auditionAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
     }
 
 
